b0d2f59293763daabf1fdd6560743707622635dd
Corey Besmer <cs.utexas.edu>
2013-02-28 17:47:41 -0600

    Added valid() function. Working on allocate() now.

--- Allocator.h
+++ Allocator.h
@@ -21,131 +21,180 @@
 
 template <typename T, int N>
 class Allocator {
-    public:
-        // --------
-        // typedefs
-        // --------
+public:
+    // --------
+    // typedefs
+    // --------
 
-        typedef T                 value_type;
+    typedef T                 value_type;
 
-        typedef int               size_type;
-        typedef std::ptrdiff_t    difference_type;
+    typedef int               size_type;
+    typedef std::ptrdiff_t    difference_type;
 
-        typedef value_type*       pointer;
-        typedef const value_type* const_pointer;
+    typedef value_type*       pointer;
+    typedef const value_type* const_pointer;
 
-        typedef value_type&       reference;
-        typedef const value_type& const_reference;
+    typedef value_type&       reference;
+    typedef const value_type& const_reference;
 
-    public:
-        // -----------
-        // operator ==
-        // -----------
+public:
+    // -----------
+    // operator ==
+    // -----------
 
-        friend bool operator == (const Allocator&, const Allocator&) {
-            return true;}
+    friend bool operator == (const Allocator&, const Allocator&) {
+        return true;
+    }
 
-        // -----------
-        // operator !=
-        // -----------
+    // -----------
+    // operator !=
+    // -----------
 
-        friend bool operator != (const Allocator& lhs, const Allocator& rhs) {
-            return !(lhs == rhs);}
+    friend bool operator != (const Allocator& lhs, const Allocator& rhs) {
+        return !(lhs == rhs);
+    }
 
-    private:
-        // ----
-        // data
-        // ----
+private:
+    // ----
+    // data
+    // ----
 
-        char a[N];
+    char a[N];
 
-        // -----
-        // valid
-        // -----
+    // -----
+    // valid
+    // -----
 
-        /**
-         * O(1) in space
-         * O(n) in time
-         * <your documentation>
-         */
-        bool valid () const {
-            // <your code>
-            return true;}
+    /**
+     * O(1) in space
+     * O(n) in time
+     * Goes through allocated memory ensuring that all 
+	 * spaces have been set up with the correct sentinels.
+     */
+    bool valid () const {
+        size_type cur = 0, pos = 0;
+		while (cur < N) {
+			cur = view(*(a+pos));
+			if (cur < 1) cur *= -1;
+			if (cur != view(*(a+pos+cur+4))) return false;
+			pos += (cur + 8);
+		}
+        return true;
+    }
 
-    public:
-        // ------------
-        // constructors
-        // ------------
+public:
+    // ------------
+    // constructors
+    // ------------
 
-        /**
-         * O(1) in space
-         * O(1) in time
-         * <your documentation>
-         */
-        Allocator () {
-            // <your code>
-            assert(valid());}
+    /**
+     * O(1) in space
+     * O(1) in time
+     * <your documentation>
+     */
+    Allocator () {
+        // <your code>
+        assert(valid());
+    }
 
-        // Default copy, destructor, and copy assignment
-        // Allocator  (const Allocator<T>&);
-        // ~Allocator ();
-        // Allocator& operator = (const Allocator&);
+    // Default copy, destructor, and copy assignment
+    // Allocator  (const Allocator<T>&);
+    // ~Allocator ();
+    // Allocator& operator = (const Allocator&);
 
-        // --------
-        // allocate
-        // --------
+    // --------
+    // allocate
+    // --------
 
-        /**
-         * O(1) in space
-         * O(n) in time
-         * <your documentation>
-         * after allocation there must be enough space left for a valid block
-         * the smallest allowable block is sizeof(T) + (2 * sizeof(int))
-         * choose the first block that fits
-         */
-        pointer allocate (size_type n) {
-            // <your code>
-            assert(valid());
-            return 0;}                   // replace!
+    /**
+     * O(1) in space
+     * O(n) in time
+     * <your documentation>
+     * after allocation there must be enough space left for a valid block
+     * the smallest allowable block is sizeof(T) + (2 * sizeof(int))
+     * choose the first block that fits
+     */
+    pointer allocate (size_type n) {
+		assert(n > 0);
+		size_type pos;
+        size_type cur = 0;
+		while (cur < N) {
+			cur = view(*(a+pos));
+			if (cur < 1) {
+				pos += (cur * -1 + 8); // skip taken block
+				continue; // move on to next block
+			}
+			if (cur >= n && pos + cur + 8 < N) {
+				pointer block = a+pos;
+				*(a+pos) = -n;
+				if (N-(pos+cur+8) < sizeof(T)+8) {// give them all
+					*(pos+cur+4) = -n;
+				}
+				else { // give them what they need and fix sentinels
+					*(pos+n+4) = -n;
+				}
+				return block;
+			}
+		}
+		
+        assert(valid());
+        return 0;
+    }                   // replace!
 
-        // ---------
-        // construct
-        // ---------
+    // ---------
+    // construct
+    // ---------
 
-        /**
-         * O(1) in space
-         * O(1) in time
-         * <your documentation>
-         */
-        void construct (pointer p, const_reference v) {
-            // new (p) T(v);                            // uncomment!
-            assert(valid());}
+    /**
+     * O(1) in space
+     * O(1) in time
+     * <your documentation>
+     */
+    void construct (pointer p, const_reference v) {
+        // new (p) T(v);                            // uncomment!
+        assert(valid());
+    } 
 
-        // ----------
-        // deallocate
-        // ----------
+    // ----------
+    // deallocate
+    // ----------
 
-        /**
-         * O(1) in space
-         * O(1) in time
-         * <your documentation>
-         * after deallocation adjacent free blocks must be coalesced
-         */
-        void deallocate (pointer p, size_type = 0) {
-            // <your code>
-            assert(valid());}
+    /**
+     * O(1) in space
+     * O(1) in time
+     * <your documentation>
+     * after deallocation adjacent free blocks must be coalesced
+     */
+    void deallocate (pointer p, size_type = 0) {
+        // <your code>
+        assert(valid());
+    } 
 
-        // -------
-        // destroy
-        // -------
+    // -------
+    // destroy
+    // -------
 
-        /**
-         * O(1) in space
-         * O(1) in time
-         * <your documentation>
-         */
-        void destroy (pointer p) {
-            // p->~T();            // uncomment!
-            assert(valid());}};
+    /**
+     * O(1) in space
+     * O(1) in time
+     * <your documentation>
+     */
+    void destroy (pointer p) {
+        // p->~T();            // uncomment!
+        assert(valid());
+    }
+	
+	// ----
+	// view
+	// ----
+	
+	/**
+	 * Take a byte, return it and the following 4 bytes.
+	 *
+	 */
+	int& view (char& c) {
+		return *reinterpret_cast<int*>(&c);
+	}
+};
 
 #endif // Allocator_h
